40b7ef80c6133e03df8c5792a4fa5a94eff649da
Ugly-gram <139082706+Ugly-gram@users.noreply.github.com>
2024-11-19 18:29:13 +0100

    Add files via upload

---
+++ busayo.js
@@ -0,0 +1,13 @@
+function busayo(){
+    let htmlcode = document.getElementById('html-code');
+    let csscode = document.getElementById('css-code');
+    let jscode = document.getElementById('js-code');
+
+    let output = document.getElementById('output')
+
+     output.contentDocument.body.innerHTML = htmlcode.value + "<style>" + csscode.value +"</style>" ;
+
+     output.contentWindow.eval(jscode.value);
+
+}
+busayo()
